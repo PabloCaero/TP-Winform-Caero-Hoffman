2a8d92e72904c9e94a26b6aae722db1c23bf76e0
PabloCaero <pablo_ieme@hotmail.com>
2021-09-11 14:43:28 -0300

    Comienzo codeo de Clases y Conexion a DB

---
+++ TP-Caero-Hoffman/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Caero_Hoffman
+{
+    class Articulo
+    {
+        public int IDArticulo { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        //public int IDMarca { get; set; } //TIENE QUE SER ASOCIACION
+       // public int IDCategoria { get; set; } //TIENE QUE SER ASOCIACION
+        public string ImagenURL { get; set; }
+        public float Precio { get; set; }
+    }
+}
---
+++ TP-Caero-Hoffman/ArticuloNegocio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient; //LIBRERIA PARA BASE DE DATOS
+
+namespace TP_Caero_Hoffman
+{
+    class ArticuloNegocio
+    {
+        //PUBLIC: PUEDE SER ACCEDIDA DESDE EL EXTERIOR
+        public List<Articulo> listar() //METODO PARA LISTAR LOS ARTICULOS
+        {
+            List<Articulo> lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection(); //OBJETO PARA CONECTAR A ALGO
+            SqlCommand comando = new SqlCommand(); //PARA REALIZAR ACCIONES
+            SqlDataReader lector; //RESULTADO EN LA LECTURA
+
+            //HAY QUE CREAR LAS COSAS PARA ESTABLECER LA CONEXION A DB
+            //MANEJO DE EXCEPCIONES
+
+            try
+            {
+                //SE CONFIGURAN TODOS LOS OBJETOS CREADOS
+                conexion.ConnectionString = "server = .\\SQLEXPRESS; database = CATALOGO_DB; integrated security = true";
+                //ACA PONGO TODA LA FUNCIONALIDAD QUE PUEDE FALLAR (DB, ETC)
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+         
+            return lista;
+
+        }
+    }
+}
